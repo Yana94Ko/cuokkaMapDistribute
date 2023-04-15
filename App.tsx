f7fff466ae0226beabb8bc864d561c1074c05ff7
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2023-04-15 10:58:19 +0900

    2차스크럼 2차 QA 반영 - 2

---
+++ App.tsx
@@ -0,0 +1,59 @@
+import React, {useState, useEffect} from 'react';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import HomePage from "../../../src/main/webapp/src/pages/HomePage";
+import MyPageList from "../../../src/main/webapp/src/components/home/header/MyPageList";
+import KakaoOauth2RedirectHandler from "../../../src/main/webapp/src/components/KakaoOauth2RedirectHandler";
+import {useDispatch} from "react-redux";
+import {setIsLoggedin, setUserId} from "../../../src/main/webapp/src/modules/userReducer";
+import Banner from "../../../src/main/webapp/src/components/Banner";
+import MyPage from "../../../src/main/webapp/src/pages/Mypage";
+import AdminPage from "../../../src/main/webapp/src/pages/AdminPage";
+import {setIsOpenedMyPageList} from "../../../src/main/webapp/src/modules/viewReducer";
+
+function App() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+         if(sessionStorage.getItem("id") !== null){
+             dispatch(setIsLoggedin(true));
+             dispatch(setUserId(sessionStorage.getItem("id")));
+         }
+         // else {
+         //     dispatch(setIsLoggedin(false));
+         //     dispatch(setUserId(""));
+         // }
+    });
+
+    //배너를 띄워주기 위한 state
+    const [openBanner, setOpenBanner] = useState<boolean>(false);
+
+    useEffect(() => {
+        setOpenBanner(true);
+    },[])
+
+    const onClickHandler = (e: React.MouseEvent<HTMLDivElement>) => {
+        if (e.target instanceof Element) {
+            if (typeof e.target.className === "string" && e.target.className !== "" && e.target.className.includes("myPageList")) {
+                return;
+            } else {
+                dispatch(setIsOpenedMyPageList(false))
+            }
+        }
+    }
+
+    return (
+        <BrowserRouter>
+            <div onClick={onClickHandler}>
+            <Routes>
+                <Route path="/" element={<HomePage />}/>
+                <Route path="/mypage" element={<MyPage />}/>
+                {/*↓로그인 콜백 라우팅용*/}
+                <Route path="/login/callback" element={<KakaoOauth2RedirectHandler/>}></Route>
+            </Routes>
+            {openBanner && <Banner setOpenBanner={setOpenBanner}/>}
+            </div>
+        </BrowserRouter>
+    );
+}
+
+export default App;
